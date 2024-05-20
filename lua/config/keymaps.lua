6f584584cfa92531144e7645804ff7f3b12d8601
SLuo5991 <sluo5991@gmail.com>
2024-05-20 13:19:21 -0400

    Added python lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Toggle Term
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>bT",
+  ":ToggleTerm direction=horizontal <CR>",
+  { desc = "Open horizontal terminal split" }
+)
