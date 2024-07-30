02fa6b6c8fa6095f4e936ce8e46a6239644e59fb
SLuo5991 <sluo5991@gmail.com>
2024-07-30 19:08:40 -0400

    added support for web dev and added keybind to add two terminals to toggle term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,14 +3,14 @@
 -- Add any additional keymaps here
 
 -- Toggle Term
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>bT",
-  ":ToggleTerm direction=horizontal <CR>",
-  { desc = "Open horizontal terminal split" }
-)
+vim.api.nvim_set_keymap("n", "<leader>t1", ":1ToggleTerm <CR>", { desc = "Open First Terminal" })
+vim.api.nvim_set_keymap("n", "<leader>t2", ":2ToggleTerm <CR>", { desc = "Open Second Terminal" })
 
 -- Flutter Tools
 vim.api.nvim_set_keymap("n", "<leader>Fe", ":FlutterEmulators <CR>", { desc = "Flutter Emulators" })
 vim.api.nvim_set_keymap("n", "<leader>Fr", ":FlutterRun <CR>", { desc = "Flutter Run" })
 vim.api.nvim_set_keymap("n", "<leader>Fq", ":FlutterQuit <CR>", { desc = "Flutter Quit" })
+
+-- Split Window
+vim.api.nvim_set_keymap("n", "sv", ":split <CR>", { desc = "Split Vertical" })
+vim.api.nvim_set_keymap("n", "ss", ":vsplit <CR>", { desc = "Split Horizontal" })
