956e61833730498d6fc4916e6e0ba3bb2cd81270
SLuo5991 <sluo5991@gmail.com>
2024-10-23 10:54:38 -0400

    updated plugins
    - updated flutter.lua plugin adding lsp hover
    - updated nvim-cmp

--- lua/plugins/extras/lang/flutter.lua
+++ lua/plugins/extras/lang/flutter.lua
@@ -3,7 +3,15 @@ return {
     "akinsho/flutter-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("flutter-tools").setup({})
+      require("flutter-tools").setup({
+        lsp = {
+          on_attach = function(_, bufnr)
+            -- Keymap for hover
+            local opts = { noremap = true, silent = true }
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+          end,
+        },
+      })
     end,
   },
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,10 +3,56 @@ return {
     "hrsh7th/nvim-cmp",
     event = { "InsertEnter", "CmdLineEnter" },
     dependencies = {
-      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-cmdline", -- For command-line completion
+      "hrsh7th/cmp-nvim-lsp", -- LSP completion source
+      "hrsh7th/cmp-buffer", -- Buffer completions
+      "hrsh7th/cmp-path", -- File path completions
+      "L3MON4D3/LuaSnip", -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- Snippet completions
     },
     opts = function(_, opts)
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      -- Set up cmp for Insert mode (general code completion)
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body) -- For snippet support
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" }, -- LSP completions (for Flutter, Dart, etc.)
+          { name = "luasnip" }, -- Snippet completions
+          { name = "buffer" }, -- Buffer completions
+          { name = "path" }, -- File path completions
+        }),
+      })
+
       -- `:` cmdline setup.
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
